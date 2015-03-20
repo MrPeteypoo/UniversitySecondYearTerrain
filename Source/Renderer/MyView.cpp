0bddfed7254c35da96a48034c86cd10b5dfb8878
Simon Peter Campbell <storm20200@gmail.com>
2015-03-20 14:32:33 +0000

    Organised and cleaned up the framework code a little.

--- Source/Renderer/MyView.cpp
+++ Source/Renderer/MyView.cpp
@@ -1,40 +1,58 @@
 #include "MyView.hpp"
+
+
+// STL headers.
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+
+// Engine headers.
 #include <tygra/FileHelper.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <iostream>
-#include <vector>
-#include <algorithm>
 
-MyView::MyView()
+
+// Constants.
+const int kVertexPosition { 0 },
+          kVertexNormal   { 1 };
+
+
+/////////////////////////////////
+// Constructors and destructor //
+/////////////////////////////////
+
+MyView::MyView (MyView&& move)
 {
+    *this = std::move (move);
 }
 
-MyView::~MyView()
+
+MyView& MyView::operator= (MyView&& move)
 {
+    if (this != &move)
+    {
+        // TODO: Write the move constructor.
+    }
+
+    return *this;
 }
 
-void MyView::setScene (std::shared_ptr<const SceneModel::Context> scene)
-{
-    scene_ = scene;
-}
 
-void MyView::toggleShading()
-{
-    shade_normals_ = !shade_normals_;
-}
+////////////////////
+// Initialisation //
+////////////////////
 
 void MyView::windowViewWillStart (std::shared_ptr<tygra::Window> window)
 {
-    assert (scene_ != nullptr);
     frameworkLoading();
 
     // Generate the terrain and prepare static batching.
-    const float sizeX = scene_->getTerrainSizeX();
-    const float sizeY = scene_->getTerrainSizeY();
-    const float sizeZ = scene_->getTerrainSizeZ();
+    const float sizeX = m_scene->getTerrainSizeX();
+    const float sizeY = m_scene->getTerrainSizeY();
+    const float sizeZ = m_scene->getTerrainSizeZ();
 
-    tygra::Image height_image = tygra::imageFromPNG(scene_->getTerrainHeightMapName());
+    tygra::Image height_image = tygra::imageFromPNG(m_scene->getTerrainHeightMapName());
 
 	// below is an example of reading the red component of pixel(x,y) as a byte [0,255]
 	//uint8_t height = *(uint8_t*)height_image(x, y);
@@ -46,24 +64,24 @@ void MyView::windowViewWillStart (std::shared_ptr<tygra::Window> window)
     //const auto& elements = mesh.GetElementArray();
     //const auto& positions = mesh.GetPositionArray();
 
-    //glGenBuffers(1, &terrain_mesh_.element_vbo);
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, terrain_mesh_.element_vbo);
+    //glGenBuffers(1, &m_terrainMesh.element_vbo);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_terrainMesh.element_vbo);
     //glBufferData(GL_ELEMENT_ARRAY_BUFFER,
     //    elements.size() * sizeof(unsigned int),
     //    elements.data(), GL_STATIC_DRAW);
     //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    //terrain_mesh_.element_count = elements.size();
+    //m_terrainMesh.element_count = elements.size();
 
-    //glGenBuffers(1, &terrain_mesh_.position_vbo);
-    //glBindBuffer(GL_ARRAY_BUFFER, terrain_mesh_.position_vbo);
+    //glGenBuffers(1, &m_terrainMesh.position_vbo);
+    //glBindBuffer(GL_ARRAY_BUFFER, m_terrainMesh.position_vbo);
     //glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(glm::vec3),
     //             positions.data(), GL_STATIC_DRAW);
     //glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    //glGenVertexArrays(1, &terrain_mesh_.vao);
-    //glBindVertexArray(terrain_mesh_.vao);
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, terrain_mesh_.element_vbo);
-    //glBindBuffer(GL_ARRAY_BUFFER, terrain_mesh_.position_vbo);
+    //glGenVertexArrays(1, &m_terrainMesh.vao);
+    //glBindVertexArray(m_terrainMesh.vao);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_terrainMesh.element_vbo);
+    //glBindBuffer(GL_ARRAY_BUFFER, m_terrainMesh.position_vbo);
     //glEnableVertexAttribArray(kVertexPosition);
     //glVertexAttribPointer(kVertexPosition, 3, GL_FLOAT, GL_FALSE,
     //                      sizeof(glm::vec3), TGL_BUFFER_OFFSET(0));
@@ -71,106 +89,11 @@ void MyView::windowViewWillStart (std::shared_ptr<tygra::Window> window)
     //glBindVertexArray(0);
 }
 
-void MyView::windowViewDidReset (std::shared_ptr<tygra::Window> window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-void MyView::windowViewDidStop (std::shared_ptr<tygra::Window> window)
-{
-    glDeleteProgram(terrain_sp_);
-    glDeleteProgram(shapes_sp_);
-
-    glDeleteBuffers(1, &terrain_mesh_.position_vbo);
-    glDeleteBuffers(1, &terrain_mesh_.element_vbo);
-    glDeleteVertexArrays(1, &terrain_mesh_.vao);
-}
-
-void MyView::windowViewRender (std::shared_ptr<tygra::Window> window)
-{
-    assert(scene_ != nullptr);
-
-    GLint viewport[4];
-    glGetIntegerv(GL_VIEWPORT, viewport);
-    const float aspect_ratio = viewport[2] / (float)viewport[3];
-
-    const auto& camera = scene_->getCamera();
-    glm::mat4 projection_xform = glm::perspective(camera.getVerticalFieldOfViewInDegrees(),
-                                                  aspect_ratio,
-                                                  camera.getNearPlaneDistance(),
-                                                  camera.getFarPlaneDistance());
-    glm::vec3 camera_pos = camera.getPosition();
-    glm::vec3 camera_at = camera.getPosition() + camera.getDirection();
-    glm::vec3 world_up{ 0, 1, 0 };
-    glm::mat4 view_xform = glm::lookAt(camera_pos, camera_at, world_up);
-
-
-    /* TODO: you are free to modify any of the drawing code below */
-
-
-    glClearColor(0.f, 0.f, 0.25f, 0.f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glPolygonMode(GL_FRONT_AND_BACK, shade_normals_ ? GL_FILL : GL_LINE);
-
-    glUseProgram(terrain_sp_);
-
-    GLuint shading_id = glGetUniformLocation(terrain_sp_, "use_normal");
-    glUniform1i(shading_id, shade_normals_);
-
-    glm::mat4 world_xform = glm::mat4(1);
-    glm::mat4 view_world_xform = view_xform * world_xform;
-
-    GLuint projection_xform_id = glGetUniformLocation(terrain_sp_,
-                                                      "projection_xform");
-    glUniformMatrix4fv(projection_xform_id, 1, GL_FALSE,
-                       glm::value_ptr(projection_xform));
-
-    GLuint view_world_xform_id = glGetUniformLocation(terrain_sp_,
-                                                      "view_world_xform");
-    glUniformMatrix4fv(view_world_xform_id, 1, GL_FALSE,
-                       glm::value_ptr(view_world_xform));
-
-    glBindVertexArray(terrain_mesh_.vao);
-    glDrawElements(GL_TRIANGLES, terrain_mesh_.element_count, GL_UNSIGNED_INT, 0);
-
-
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    glUseProgram(shapes_sp_);
-
-    projection_xform_id = glGetUniformLocation(shapes_sp_,
-                                               "projection_xform");
-    glUniformMatrix4fv(projection_xform_id, 1, GL_FALSE,
-                       glm::value_ptr(projection_xform));
-
-    glBindVertexArray(cube_vao_);
-
-    for (const auto& pos : scene_->getAllShapePositions())
-    {
-        world_xform = glm::translate(glm::mat4(1), glm::vec3(pos.x, 64, -pos.y));
-        view_world_xform = view_xform * world_xform;
-
-        view_world_xform_id = glGetUniformLocation(shapes_sp_,
-                                                   "view_world_xform");
-        glUniformMatrix4fv(view_world_xform_id, 1, GL_FALSE,
-                           glm::value_ptr(view_world_xform));
-
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
-}
 
 void MyView::frameworkLoading()
 {
     GLint compile_status = 0;
-    GLint link_status = 0;
-
-    
+    GLint link_status = 0;    
     GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     std::string vertex_shader_string = tygra::stringFromFile("terrain_vs.glsl");
     const char *vertex_shader_code = vertex_shader_string.c_str();
@@ -183,8 +106,7 @@ void MyView::frameworkLoading()
         GLchar log[string_length]= "";
         glGetShaderInfoLog(vertex_shader, string_length, NULL, log);
         std::cerr << log << std::endl;
-    }
- 
+    } 
     GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     std::string fragment_shader_string = tygra::stringFromFile("terrain_fs.glsl");
     const char *fragment_shader_code = fragment_shader_string.c_str();
@@ -198,23 +120,19 @@ void MyView::frameworkLoading()
         glGetShaderInfoLog(fragment_shader, string_length, NULL, log);
         std::cerr << log << std::endl;
     }
-
-    terrain_sp_ = glCreateProgram();
-    glAttachShader(terrain_sp_, vertex_shader);
+    m_terrainShader = glCreateProgram();
+    glAttachShader(m_terrainShader, vertex_shader);
     glDeleteShader(vertex_shader);
-    glAttachShader(terrain_sp_, fragment_shader);
+    glAttachShader(m_terrainShader, fragment_shader);
     glDeleteShader(fragment_shader);
-    glLinkProgram(terrain_sp_);
-
-    glGetProgramiv(terrain_sp_, GL_LINK_STATUS, &link_status);
+    glLinkProgram(m_terrainShader);
+    glGetProgramiv(m_terrainShader, GL_LINK_STATUS, &link_status);
     if (link_status != GL_TRUE) {
         const int string_length = 1024;
         GLchar log[string_length]= "";
-        glGetProgramInfoLog(terrain_sp_, string_length, NULL, log);
+        glGetProgramInfoLog(m_terrainShader, string_length, NULL, log);
         std::cerr << log << std::endl;
     }
-
-
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     vertex_shader_string = tygra::stringFromFile("shapes_vs.glsl");
     vertex_shader_code = vertex_shader_string.c_str();
@@ -228,7 +146,6 @@ void MyView::frameworkLoading()
         glGetShaderInfoLog(vertex_shader, string_length, NULL, log);
         std::cerr << log << std::endl;
     }
-
     fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     fragment_shader_string = tygra::stringFromFile("shapes_fs.glsl");
     fragment_shader_code = fragment_shader_string.c_str();
@@ -242,27 +159,23 @@ void MyView::frameworkLoading()
         glGetShaderInfoLog(fragment_shader, string_length, NULL, log);
         std::cerr << log << std::endl;
     }
-
-    shapes_sp_ = glCreateProgram();
-    glAttachShader(shapes_sp_, vertex_shader);
+    m_shapesShader = glCreateProgram();
+    glAttachShader(m_shapesShader, vertex_shader);
     glDeleteShader(vertex_shader);
-    glAttachShader(shapes_sp_, fragment_shader);
+    glAttachShader(m_shapesShader, fragment_shader);
     glDeleteShader(fragment_shader);
-    glLinkProgram(shapes_sp_);
-
-    glGetProgramiv(shapes_sp_, GL_LINK_STATUS, &link_status);
+    glLinkProgram(m_shapesShader);
+    glGetProgramiv(m_shapesShader, GL_LINK_STATUS, &link_status);
     if (link_status != GL_TRUE) {
         const int string_length = 1024;
         GLchar log[string_length] = "";
-        glGetProgramInfoLog(terrain_sp_, string_length, NULL, log);
+        glGetProgramInfoLog(m_terrainShader, string_length, NULL, log);
         std::cerr << log << std::endl;
     }
-
-
-    glGenVertexArrays(1, &cube_vao_);
-    glBindVertexArray(cube_vao_);
-    glGenBuffers(1, &cube_vbo_);
-    glBindBuffer(GL_ARRAY_BUFFER, cube_vbo_);
+    glGenVertexArrays(1, &m_cubeVAO);
+    glBindVertexArray(m_cubeVAO);
+    glGenBuffers(1, &m_cubeVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, m_cubeVBO);
     float cube_vertices[] = {
         -0.5f, 0.f, -0.5f,   0.5f, 0.f, -0.5f,   0.5f, 0.f,  0.5f,
         -0.5f, 0.f, -0.5f,   0.5f, 0.f,  0.5f,  -0.5f, 0.f,  0.5f,
@@ -284,4 +197,105 @@ void MyView::frameworkLoading()
         sizeof(glm::vec3), TGL_BUFFER_OFFSET(0));
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+}
+
+
+//////////////
+// Clean up //
+//////////////
+
+void MyView::windowViewDidStop (std::shared_ptr<tygra::Window> window)
+{
+    glDeleteProgram (m_terrainShader);
+    glDeleteProgram (m_shapesShader);
+
+    glDeleteBuffers (1, &m_terrainMesh.position_vbo);
+    glDeleteBuffers (1, &m_terrainMesh.element_vbo);
+    glDeleteVertexArrays (1, &m_terrainMesh.vao);
+}
+
+
+///////////////
+// Rendering //
+///////////////
+
+void MyView::windowViewDidReset (std::shared_ptr<tygra::Window> window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+
+void MyView::windowViewRender (std::shared_ptr<tygra::Window> window)
+{
+    // Framework crap, not allowed to touch.
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    const float aspect_ratio = viewport[2] / (float)viewport[3];
+    const auto& camera = m_scene->getCamera();
+    glm::mat4 projection_xform = glm::perspective(camera.getVerticalFieldOfViewInDegrees(),
+                                                  aspect_ratio,
+                                                  camera.getNearPlaneDistance(),
+                                                  camera.getFarPlaneDistance());
+    glm::vec3 camera_pos = camera.getPosition();
+    glm::vec3 camera_at = camera.getPosition() + camera.getDirection();
+    glm::vec3 world_up{ 0, 1, 0 };
+    glm::mat4 view_xform = glm::lookAt(camera_pos, camera_at, world_up);
+
+
+    // Personal crap, I can touch this as much as I want *wink* *wink*.
+    glClearColor(0.f, 0.f, 0.25f, 0.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glPolygonMode(GL_FRONT_AND_BACK, m_shadeNormals ? GL_FILL : GL_LINE);
+
+    glUseProgram(m_terrainShader);
+
+    GLuint shading_id = glGetUniformLocation(m_terrainShader, "use_normal");
+    glUniform1i(shading_id, m_shadeNormals);
+
+    glm::mat4 world_xform = glm::mat4(1);
+    glm::mat4 view_world_xform = view_xform * world_xform;
+
+    GLuint projection_xform_id = glGetUniformLocation(m_terrainShader,
+                                                      "projection_xform");
+    glUniformMatrix4fv(projection_xform_id, 1, GL_FALSE,
+                       glm::value_ptr(projection_xform));
+
+    GLuint view_world_xform_id = glGetUniformLocation(m_terrainShader,
+                                                      "view_world_xform");
+    glUniformMatrix4fv(view_world_xform_id, 1, GL_FALSE,
+                       glm::value_ptr(view_world_xform));
+
+    glBindVertexArray(m_terrainMesh.vao);
+    glDrawElements(GL_TRIANGLES, m_terrainMesh.element_count, GL_UNSIGNED_INT, 0);
+
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    glUseProgram(m_shapesShader);
+
+    projection_xform_id = glGetUniformLocation(m_shapesShader,
+                                               "projection_xform");
+    glUniformMatrix4fv(projection_xform_id, 1, GL_FALSE,
+                       glm::value_ptr(projection_xform));
+
+    glBindVertexArray(m_cubeVAO);
+
+    for (const auto& pos : m_scene->getAllShapePositions())
+    {
+        world_xform = glm::translate(glm::mat4(1), glm::vec3(pos.x, 64, -pos.y));
+        view_world_xform = view_xform * world_xform;
+
+        view_world_xform_id = glGetUniformLocation(m_shapesShader,
+                                                   "view_world_xform");
+        glUniformMatrix4fv(view_world_xform_id, 1, GL_FALSE,
+                           glm::value_ptr(view_world_xform));
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 }
--- Source/Renderer/MyView.hpp
+++ Source/Renderer/MyView.hpp
@@ -1,42 +1,83 @@
 #pragma once
 
+
 #include <SceneModel/Context.hpp>
 #include <tygra/WindowViewDelegate.hpp>
 #include <tgl/tgl.h>
 #include <glm/glm.hpp>
 
-class MyView : public tygra::WindowViewDelegate
+
+
+/// <summary>
+/// The renderer used to show terrain generation and frustrum culling functionality.
+/// </summary>
+class MyView final : public tygra::WindowViewDelegate
 {
     public:
 
-        MyView();
+        /////////////////////////////////
+        // Constructors and destructor //
+        /////////////////////////////////
 
-        ~MyView();
+        MyView()                                = default;
+        ~MyView()                               = default;
 
-        void setScene(std::shared_ptr<const SceneModel::Context> scene);
+        MyView (MyView&& move);
+        MyView& operator= (MyView&& move);
 
-        void toggleShading();
+        MyView (const MyView& copy)             = delete;
+        MyView& operator= (const MyView& copy)  = delete;
+
+
+        /////////////////////////
+        // Getters and setters //
+        /////////////////////////
+
+        /// <summary> Set the SceneModel::Context used when drawing the scene. </summary>
+        /// <param name="scene"> The scene data to use. </param>
+        void setScene (const std::shared_ptr<const SceneModel::Context>& scene) { m_scene = scene; }
+
+        /// <summary> 
+        /// Toggle the shading model used by the application, this will determine whether to shade the
+        /// terrain using the normal vectors.
+        /// </summary>
+        void toggleShading() { m_shadeNormals = !m_shadeNormals; }
 
     private:
 
-        void windowViewWillStart (std::shared_ptr<tygra::Window> window) override;
+        ////////////////////
+        // Initialisation //
+        ////////////////////
 
-        void windowViewDidReset (std::shared_ptr<tygra::Window> window, int width, int height) override;
+        /// <summary> Generates smooth terrain based on the information contained in a heightmap. </summary>
+        void windowViewWillStart (std::shared_ptr<tygra::Window> window) override final;
 
-        void windowViewDidStop (std::shared_ptr<tygra::Window> window) override;
-
-        void windowViewRender (std::shared_ptr<tygra::Window> window) override;
-
+        /// <summary> This contains all of the framework-specific loading code. </summary>
         void frameworkLoading();
 
-    private:
 
-        std::shared_ptr<const SceneModel::Context> scene_;
+        //////////////
+        // Clean up //
+        //////////////
 
-        GLuint terrain_sp_{ 0 };
-        GLuint shapes_sp_{ 0 };
+        /// <summary> Cleans up the dynamic CPU and GPU memory in use by the view. </summary>
+        void windowViewDidStop (std::shared_ptr<tygra::Window> window) override final;
 
-        bool shade_normals_{ false };
+
+        ///////////////
+        // Rendering //
+        ///////////////
+
+        /// <summary> Updates the viewport with the given values. </summary>
+        void windowViewDidReset (std::shared_ptr<tygra::Window> window, int width, int height) override final;
+
+        /// <summary> Renders the terrain and cube objects using culling optimisation techniques. </summary>
+        void windowViewRender (std::shared_ptr<tygra::Window> window) override final;
+
+
+        ///////////////////
+        // Internal data //
+        ///////////////////
 
         struct MeshGL
         {
@@ -45,15 +86,17 @@ class MyView : public tygra::WindowViewDelegate
             GLuint vao{ 0 };
             int element_count{ 0 };
         };
-        MeshGL terrain_mesh_;
 
-        enum
-        {
-            kVertexPosition = 0,
-            kVertexNormal = 1,
-        };
+        GLuint                                      m_terrainShader { 0 };          //!< The shader program used for drawing the terrain.
+        GLuint                                      m_shapesShader  { 0 };          //!< The shader program used for drawing the static cubes.
+        
+        MeshGL                                      m_terrainMesh   { };            //!< The mesh containing information required to draw the terrain.
 
-	    GLuint cube_vao_{ 0 };
-	    GLuint cube_vbo_{ 0 };
+	    GLuint                                      m_cubeVAO       { 0 };          //!< The ID of the VAO containing information relating to the drawing of a cube.
+	    GLuint                                      m_cubeVBO       { 0 };          //!< The ID of the VBO containing the vertices of a cube.
+
+        bool                                        m_shadeNormals  { false };      //!< Determines whether the terrain should be shaded using its normal vector.
+        
+        std::shared_ptr<const SceneModel::Context>  m_scene         { nullptr };    //!< A poiner to the context used for camera information when rendering the scene.
 
 };
